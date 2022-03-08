c21b4f337f0a7ce550702d819181af099127f3ff
csio_123 <abnitchauhanabnit@gmail.com>
2022-03-08 17:26:42 +0530

    Connected with MongoDB Client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const {MongoClient} = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 
-const URL = process.env.API_URL;
+  const uri = process.env.API_URL;
+
+ 
+
+const { MongoClient, ServerApiVersion } = require('mongodb'); 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("users").collection("data");
+
+  result = collection.find({}).toArray(function(err, result){
+      if(err) throw err;
+      console.log(result)
+      client.close();
+  })
+  // perform actions on the collection object
+    
+});
 
 app.get('/', (req, res) =>{
     console.log("GET REQUEST WORKING FOR HOMEPAGE");
